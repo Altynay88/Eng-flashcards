b7ee80077c9fd0f62e228df13c5d84cd848aa020
Altynay <altynaygoha2007@mail.ru>
2022-02-13 15:46:19 +0600

    split cardlist and word

---
+++ src/components/Word.jsx
@@ -0,0 +1,90 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+import "./flashcard.css";
+import { useState } from "react";
+
+function Word({ english, russian, transcription }) {
+  const [isEditBtn, changeEditBtn] = useState(false);
+
+  const [word, setWord] = useState({ english, russian, transcription });
+
+  function onClick() {
+    changeEditBtn(true);
+  }
+
+  function onChangeEnglish(evt) {
+    setWord({
+      english: evt.target.value,
+      russian: word.russian,
+      transcription: word.transcription,
+    });
+  }
+
+  function onChangeRussian(evt) {
+    setWord({
+      english: word.english,
+      russian: evt.target.value,
+      transcription: word.transcription,
+    });
+  }
+
+  function onChangeTranscription(evt) {
+    setWord({
+      english: word.english,
+      russian: word.russian,
+      transcription: evt.target.value,
+    });
+  }
+
+  function onSave() {
+    changeEditBtn(false);
+  }
+
+  function onCancel(evt) {
+    setWord({
+      english,
+      russian,
+      transcription,
+    });
+    changeEditBtn(false);
+  }
+
+  if (!isEditBtn) {
+    return (
+      <tr>
+        <td>{word.english}</td>
+        <td>{word.russian}</td>
+        <td>{word.transcription}</td>
+        <td>
+          <Button onClick={onClick} variant="primary" className="primaryBtn">
+            Edit
+          </Button>
+          <Button onClick={onCancel} variant="danger">
+            Cancel
+          </Button>
+        </td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>
+          <input value={word.english} onChange={onChangeEnglish} />
+        </td>
+        <td>
+          <input value={word.russian} onChange={onChangeRussian} />
+        </td>
+        <td>
+          <input value={word.transcription} onChange={onChangeTranscription} />
+        </td>
+        <td>
+          <Button onClick={onSave} variant="success" className="successBtn">
+            Save
+          </Button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Word;
--- src/components/cardlist.jsx
+++ src/components/cardlist.jsx
@@ -1,7 +1,8 @@
-import { Table, Button } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import React from "react";
 import "./flashcard.css";
 import data from "../data.js";
-import { useState } from "react";
+import Word from "./Word";
 
 function Cardlist() {
   return (
@@ -23,90 +24,6 @@ function Cardlist() {
       </Table>
     </div>
   );
-}
-
-function Word({ id, english, russian, transcription }) {
-  const [isEditBtn, changeEditBtn] = useState(false);
-
-  const [word, setWord] = useState({ english, russian, transcription });
-
-  function onClick() {
-    changeEditBtn(true);
-  }
-
-  function onChangeEnglish(evt) {
-    setWord({
-      english: evt.target.value,
-      russian: word.russian,
-      transcription: word.transcription,
-    });
-  }
-
-  function onChangeRussian(evt) {
-    setWord({
-      english: word.english,
-      russian: evt.target.value,
-      transcription: word.transcription,
-    });
-  }
-
-  function onChangeTranscription(evt) {
-    setWord({
-      english: word.english,
-      russian: word.russian,
-      transcription: evt.target.value,
-    });
-  }
-
-  function onSave() {
-    changeEditBtn(false);
-  }
-
-  function onCancel(evt) {
-    setWord({
-      english,
-      russian,
-      transcription,
-    });
-    changeEditBtn(false);
-  }
-
-  if (!isEditBtn) {
-    return (
-      <tr>
-        <td>{word.english}</td>
-        <td>{word.russian}</td>
-        <td>{word.transcription}</td>
-        <td>
-          <Button onClick={onClick} variant="primary" className="primaryBtn">
-            Edit
-          </Button>
-          <Button onClick={onCancel} variant="danger">
-            Cancel
-          </Button>
-        </td>
-      </tr>
-    );
-  } else {
-    return (
-      <tr>
-        <td>
-          <input value={word.english} onChange={onChangeEnglish} />
-        </td>
-        <td>
-          <input value={word.russian} onChange={onChangeRussian} />
-        </td>
-        <td>
-          <input value={word.transcription} onChange={onChangeTranscription} />
-        </td>
-        <td>
-          <Button onClick={onSave} variant="success" className="successBtn">
-            Save
-          </Button>
-        </td>
-      </tr>
-    );
-  }
 }
 
 export default Cardlist;
